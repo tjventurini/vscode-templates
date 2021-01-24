1ed2f6400514b4c17e8a3fa9206b370804d80cb1
Thomas Venturini <thomas@venturini.codes>
2021-01-24 03:15:21 +0100

    update php exception template

--- templates/Exception.php
+++ templates/Exception.php
@@ -7,8 +7,12 @@ use Exception;
 
 class {{=$.NAME}} extends Exception
 {
-    public function __construct($message = null)
-    {
-        $this->message = $message;
+    /**
+     * Constructor of this custom exception.
+     * 
+     * Should have the stacktrace available.
+     */
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
     }
 }
